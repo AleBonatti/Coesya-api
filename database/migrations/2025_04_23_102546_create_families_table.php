51fdd407bd61d3350ff2cf802386caa6120cd245
AleBonatti <la.seppia@gmail.com>
2026-01-05 14:56:10 +0100

    added family handling endpoints

--- database/migrations/2025_04_23_102546_create_families_table.php
+++ database/migrations/2025_04_23_102546_create_families_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('families', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->string('code', 100);
+            $table->string('slug', 100);
+            $table->char('code', 5);
             $table->string('profile_photo_path')->nullable();
 
             $table->timestamps();
